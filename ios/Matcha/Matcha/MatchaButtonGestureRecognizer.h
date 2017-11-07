d2f14783af078f25b6a315746ae9721919ca147a
Kevin Dang <overcyn@gmail.com>
2017-11-07 00:32:18 -0800

    Remove unused code

--- ios/Matcha/Matcha/MatchaButtonGestureRecognizer.h
+++
@@ -1,9 +0,0 @@
-#import <UIKit/UIKit.h>
-@class MatchaViewController;
-@class GPBAny;
-
-@interface MatchaButtonGestureRecognizer : UIGestureRecognizer
-- (id)initWithMatchaVC:(MatchaViewController *)viewRoot viewId:(int64_t)viewId protobuf:(GPBAny *)pb;
-- (void)disable;
-- (void)updateWithProtobuf:(GPBAny *)pb;
-@end
--- ios/Matcha/Matcha/MatchaPressGestureRecognizer.h
+++
@@ -1,9 +0,0 @@
-#import <UIKit/UIKit.h>
-@class GPBAny;
-@class MatchaViewController;
-
-@interface MatchaPressGestureRecognizer : UILongPressGestureRecognizer
-- (id)initWithMatchaVC:(MatchaViewController *)viewRoot viewId:(int64_t)viewId protobuf:(GPBAny *)pb;
-- (void)updateWithProtobuf:(GPBAny *)pb;
-- (void)disable;
-@end
